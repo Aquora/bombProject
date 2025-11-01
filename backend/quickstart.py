9d9f462807940802107411a333316ea61883735a
darrenlee <cyberfan1212@gmail.com>
2025-11-01 12:46:50 -0700

    Your commit message here

--- backend/quickstart.py
+++ backend/quickstart.py
@@ -28,7 +28,7 @@ def main():
       flow = InstalledAppFlow.from_client_secrets_file(
           "credentials.json", SCOPES
       )
-      creds = flow.run_local_server(port=0)
+      creds = flow.run_local_server(port=8000)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
@@ -54,3 +54,4 @@ def main():
 
 if __name__ == "__main__":
   main()
+  
